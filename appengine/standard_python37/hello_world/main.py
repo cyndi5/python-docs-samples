5742e2a875dd091913a4ce0b51902bc81f600180
Cynthia <cynthiacavanaugh1@gmail.com>
2019-08-19 12:13:07 -0500

    webcolors added

--- appengine/standard_python37/hello_world/main.py
+++ appengine/standard_python37/hello_world/main.py
@@ -14,7 +14,9 @@
 
 # [START gae_python37_app]
 from flask import Flask
-
+from flask import request
+from webcolors import hex_to_rgb
+from webcolors import normalize_hex
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
@@ -26,6 +28,12 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
+@app.route('/rgb')
+def rgb():
+    hex = request.args.get('hex')
+    if(not hex.startswith('#')):
+        hex = '#' + hex
+    return str(hex_to_rgb(normalize_hex(hex)))
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
